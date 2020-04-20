a3913a6d092171181ec4c5d3550bd84742ec69dc
Dinesh Gopal Chand <dineshgopalchand@gmail.com>
2020-04-20 09:00:25 +0530

    Discussed Safe traversal

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 import { CoursesService } from '../course/services/courses.service';
 
 @Component({
   selector: 'app-directives',
   templateUrl: './directives.component.html',
-  styleUrls: ['./directives.component.scss']
+  styleUrls: ['./directives.component.scss'],
+  // changeDetection: ChangeDetectionStrategy.Default
 })
 export class DirectivesComponent implements OnInit {
-
+  // @Input() someValue;
   viewTab = 'course';
+
+  details = [{
+    id: 1,
+    name: {
+      firstname: 'dinesh',
+      lastname: 'Chand'
+    },
+    age: 56
+  },
+  {
+    id: 2,
+    name: {
+      firstname: 'dinesh'
+    }
+  },
+  {
+    id: 3,
+    age: 30
+  },
+  {
+    id: 4,
+    name: {
+      firstname: 'Gopal'
+    }
+  }];
 
   numberList = [1, 2, 4, 3, 2, 4, 56, 332, 2, 5, 7];
 
@@ -20,6 +46,13 @@ export class DirectivesComponent implements OnInit {
 
   get courseList() {
     return this.courseService.courseList;
+  }
+  addNewCourse(courseInput: HTMLInputElement) {
+    this.courseService.courseList.push({
+      id: Date.now(),
+      name: courseInput.value
+    });
+    courseInput.value = '';
   }
 
 }
